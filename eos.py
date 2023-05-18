d8aba511ed59b3c908ea7c8ec9454d60744dc1f5
rayAnish <133885480+rayAnish@users.noreply.github.com>
2023-05-18 21:28:50 +0530

    Add files via upload

---
+++ eos.py
@@ -0,0 +1,9 @@
+while True:
+    n=int(input("Enter the number: "))
+    if n==111:
+        print("Emergency Alert Triggered")
+        print("Emergency Alert sent to Family")
+        print("Emergency Alert sent to Police station")
+        print("Emergency Alert sent to Ambulance")
+        break
+
